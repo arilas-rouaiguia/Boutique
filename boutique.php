0fa9195e806d265794c145ce9898e6423f3de688
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-05-03 13:49:23 +0200

    Add files via upload

--- boutique.php
+++ boutique.php
@@ -3,14 +3,6 @@
 require_once('includes/header.php');
 require_once('includes/sidebar.php');
 
-if(isset($_GET['categorie'])){
-	
-	$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
-$categorie = $_GET['categorie'];
-$select = $pdo->prepare("SELECT * FROM products WHERE categorie='$categorie'");
-$select->execute();
-
-
 if(isset($_GET['show'])){
 $products = $_GET['show'];
 
@@ -18,7 +10,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
 $select = $pdo->prepare("SELECT * FROM products WHERE titre='$products'");
 $select->execute();
 
-$s=$select->fetch(PDO::FETCH_OBJ);
+$s = $select->fetch(PDO::FETCH_OBJ);
 
 $description = $s->description;
 
@@ -28,17 +20,25 @@ $description_finale = wordwrap($description,25,'<br><br>', true);
 	?><br>
 	<div style="text-align: center;">
 	<img src="admin/imgs/<?php echo $s->titre; ?>.jpg"/>
-<h1><?php echo $s->titre;?></h1>
-<h5><?php echo $description_finale;?></h5>
-<h4><?php echo $s->prix;?> Euros</h4>
+	<h1><?php echo $s->titre;?></h1>
+	<h5><?php echo $description_finale;?></h5>
+	<h4><?php echo $s->prix;?> Euros</h4>
 <a href="">Ajouter au Panier</a>
+<br><br><br><br><br><br><br><br><br><br><br><br>
 </div><br>
 
 <?php
 }else{
 	
 
-	while($s=$select->fetch(PDO::FETCH_OBJ)){
+if(isset($_GET['categorie'])){
+	
+	$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
+$categorie = $_GET['categorie'];
+$select = $pdo->prepare("SELECT * FROM products WHERE categorie='$categorie'");
+$select->execute();
+
+while($s=$select->fetch(PDO::FETCH_OBJ)){
 		
 		$lenght = 70;
 		
@@ -49,33 +49,39 @@ $description_finale = wordwrap($description,25,'<br><br>', true);
 		$description_finale=wordwrap($new_description,25,'<br><br>', true);
 		
 		?>
+		<br><br><br>
 		<a href="?show=<?php echo $s->titre; ?>"><img src="admin/imgs/<?php echo $s->titre; ?>.jpg"/></a>
 		<a href="?show=<?php echo $s->titre; ?>"><h2><?php echo $s->titre;?></h2></a>
 		<h5><?php echo $description_finale;?></h5>
 		<h4><?php echo $s->prix;?> Euros</h4>
 		<a href="">Ajouter au Panier</a><br>
-		<br><br>
+		<br><br><br>
 		
 		<?php
 	}
-}
-	
-	require_once('includes/footer.php');
-	
 }else{
+
 	
 
-$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
-$select = $pdo->query('SELECT * FROM categorie');
+	$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
+	$cat = $pdo->query('SELECT * FROM categorie');	
 
-while ($s= $select->fetch(PDO::FETCH_OBJ)){
+while ($c= $cat->fetch(PDO::FETCH_OBJ)){
 
 ?>
+<br><br><br>
+<a href="?categorie=<?php echo $c->nom;?>"> <h3><?php echo $c->nom ?></h3> </a> <br>
 
-<a href="?categorie=<?php echo $s->nom;?>"> <h3><?php echo $s->nom ?></h3> </a>
-
+<br><br><br><br><br><br><br><br><br>
 <?php
 }
+require_once('includes/footer.php');
 }
-
+}
 ?>
+<br><br><br><br>
+<footer>
+<?php
+require_once('includes/footer.php');
+?>
+</footer>
